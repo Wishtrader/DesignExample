c335884a0f0ff1e913bdb7778cabdbc51ba76628
Andrei Kamarou <mr.akomarov@gmail.com>
2021-09-05 00:54:12 +0300

    Initial Commit

---
+++ DesignExample/DesignExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignExampleApp.swift
+//  DesignExample
+//
+//  Created by Andrei Kamarou on 5.09.21.
+//
+
+import SwiftUI
+
+@main
+struct DesignExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
