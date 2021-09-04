a4c030ffd6bd35beb0e422b277a4b530d51d0d46
Andrei Kamarou <mr.akomarov@gmail.com>
2021-09-05 02:32:03 +0300

    Initial commit

--- DesignExample/ContentView.swift
+++ DesignExample/ContentView.swift
@@ -1,16 +1,38 @@
-//
-//  ContentView.swift
-//  DesignExample
-//
-//  Created by Andrei Kamarou on 5.09.21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            TitleView()
+            
+            BackCardView()
+                .background(Color(#colorLiteral(red: 1, green: 0, blue: 0.7751089334, alpha: 1)))
+                .cornerRadius(20)
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(10))
+                .rotation3DEffect(
+                    .degrees(10),
+                    axis: (x: 10.0, y: 1.0, z: 0.0)
+                )
+                .blendMode(.hardLight)
+            
+            BackCardView()
+                .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                .cornerRadius(20)
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(5))
+                .rotation3DEffect(
+                    .degrees(5),
+                    axis: (x: 10.0, y: 0.0, z: 0.0)
+                )
+                .blendMode(.hardLight)
+            
+            
+            CardView()
+        }
     }
 }
 
@@ -19,3 +41,65 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("SwiftUI DESIGN")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("Primary"))
+                }
+                
+                Spacer()
+                
+                Image("Logo SwiftUI")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            
+            Image("2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+//https://youtu.be/RyazSN_R4oI?list=PLUb9K99oQb2voS2AyA1mrKQ8c2LZTegwl&t=1342
